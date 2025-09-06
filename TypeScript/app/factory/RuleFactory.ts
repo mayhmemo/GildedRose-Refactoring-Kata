a7002fb69964d2b82c2d671caff87e6f8e38470a
Mayhm <mayhm.social@proton.me>
2025-09-06 14:30:38 -0300

    refactor: change swtich by declarative maps

--- TypeScript/app/factory/RuleFactory.ts
+++ TypeScript/app/factory/RuleFactory.ts
@@ -2,19 +2,20 @@ import { IUpdateRule } from "@/core/IUpdateRule";
 import { Item } from "@/domain/Item";
 import { AgedBrieRule, BackstageRule, NormalRule, SulfurasRule, ConjuredRule } from "@/rules";
 
+type RuleConstructor = new () => IUpdateRule;
+
+const exactItemRuleMap = new Map<string, RuleConstructor>([
+  ['Aged Brie', AgedBrieRule],
+  ['Backstage passes to a TAFKAL80ETC concert', BackstageRule],
+  ['Sulfuras, Hand of Ragnaros', SulfurasRule],
+  ['Conjured', ConjuredRule]
+]);
+
 export class RuleFactory {
   static for(item: Item): IUpdateRule {
-    switch (item.name) {
-      case 'Aged Brie':
-        return new AgedBrieRule();
-      case 'Backstage passes to a TAFKAL80ETC concert':
-        return new BackstageRule();
-      case 'Sulfuras, Hand of Ragnaros':
-        return new SulfurasRule();
-      case 'Conjured': 
-        return new ConjuredRule();
-      default:
-        return new NormalRule();
-    }
+    const ruleConstructor = exactItemRuleMap.get(item.name);
+    if (ruleConstructor) return new ruleConstructor();
+
+    return new NormalRule();
   }
 }
